a04de565dad32d9a62e0d8ebe52eacc2d902b69a
ViridiIgnis <127263221+ViridiIgnis@users.noreply.github.com>
2023-03-08 21:17:51 -0300

    Update java.js

--- java.js
+++ java.js
@@ -1,22 +1,36 @@
+/*Efecto body*/
 let bg = document.body;
-let count = 20;
+let count = window.innerWidth > 600 ? 20 : 10;
 for (let i = 0; i < count; i++){
     let glitchBox = document.createElement('div')
     glitchBox.className = 'box';
     bg.appendChild(glitchBox);
 }
-setInterval(function(){
+if (window.innerWidth > 768) {
+  setInterval(function(){
     let glitch = document.getElementsByClassName('box');
     for (let i = 0; i < glitch.length; i++){
-        glitch[i].style.left = Math.floor(Math.random()*document.body.clientWidth) + 'px';
-        glitch[i].style.top = Math.floor(Math.random()*document.body.clientHeight) + 'px';
-        glitch[i].style.width = Math.floor(Math.random()*100) + 'px';
-        glitch[i].style.height = Math.floor(Math.random()*40) + 'px';
-        glitch[i].style.backgroundPosition = Math.floor(Math.random()*50) + 'px';
+      glitch[i].style.left = Math.floor(Math.random()*document.body.clientWidth) + 'px';
+      glitch[i].style.top = Math.floor(Math.random()*document.body.clientHeight) + 'px';
+      glitch[i].style.width = Math.floor(Math.random()*100) + 'px';
+      glitch[i].style.height = Math.floor(Math.random()*40) + 'px';
+      glitch[i].style.backgroundPosition = Math.floor(Math.random()*50) + 'px';
     }
-},500)
+  },500)
+} else {
+  setInterval(function(){
+    let glitch = document.getElementsByClassName('box');
+    for (let i = 0; i < glitch.length; i++){
+      glitch[i].style.left = Math.floor(Math.random()*document.body.clientWidth) + 'px';
+      glitch[i].style.top = Math.floor(Math.random()*document.body.clientHeight) + 'px';
+      glitch[i].style.width = Math.floor(Math.random()*50) + 'px';
+      glitch[i].style.height = Math.floor(Math.random()*30) + 'px';
+      glitch[i].style.backgroundPosition = Math.floor(Math.random()*10) + 'px';
+    }
+  },500)
+}
 
-
+/*actualizar F5*/
 window.onload = function() {
     setTimeout(function() {
         scrollTo(0, 0);
